84bcd667d4157a0c8b884d2c886c8110cdeac22e
meggmaier <megg.maier@gmail.com>
2019-07-18 20:23:05 +0200

    spring boot app error

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -18,7 +18,11 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Optional<Task> getTaskById(Long id) {
+    public Optional<Task> getTaskById(final Long id) {
         return repository.findById(id);
+    }
+
+    public Task saveTask(final Task task){
+        return repository.save(task);
     }
 }
