b687796421a3f00ea9052d252848e81ba4bc7e91
meggmaier <megg.maier@gmail.com>
2019-10-11 12:58:17 +0200

    TrelloControllerTest added

---
+++ src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrelloControllerTest {
+
+    @Test
+    public void getTrelloBoards() {
+    }
+
+    @Test
+    public void createTrelloCard() {
+    }
+}
