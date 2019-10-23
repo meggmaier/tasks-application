cf4afa6b6b64d0e1fc6c2aa81fcfd0ab90dda6dc
meggmaier <megg.maier@gmail.com>
2019-10-23 19:16:47 +0200

    template error

---
+++ src/test/java/com/crud/tasks/MailCreatorServiceTest.java
@@ -0,0 +1,27 @@
+package com.crud.tasks;
+
+
+import com.crud.tasks.service.MailCreatorService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailCreatorServiceTest {
+
+    @Autowired
+    MailCreatorService mailCreatorService;
+
+    @Test
+    public void buildTrelloCardEmailTest(){
+        //Given
+        //When
+        String message = "Test message";
+        String outputHtml = mailCreatorService.buildTrelloCardEmail(message);
+        //Then
+        System.out.println(outputHtml);
+    }
+}
