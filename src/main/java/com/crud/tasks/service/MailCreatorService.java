106ec2de0253b611f4fc3b63359439cc574f453a
meggmaier <megg.maier@gmail.com>
2019-10-24 12:32:36 +0200

    other thymeleaf funcionalities added

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,48 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.trello.config.AdminConfig;
+import com.crud.tasks.trello.config.CompanyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your task");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://meggmaier.github.io/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "Sincerely, " + companyConfig.getName());
+        context.setVariable("company_details", companyConfig.getName() + ", email: " + companyConfig.getEmail() + ", phone: " + companyConfig.getPhone());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+}
